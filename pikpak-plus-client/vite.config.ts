fb58c7a9ac68f36b4485731a331639ac3ebd61cf
theUniversityFinder <ganjisai19@gmail.com>
2024-01-05 20:35:53 +0530

    @vitejs/plugin-react-swc

--- pikpak-plus-client/vite.config.ts
+++ pikpak-plus-client/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import dotenv from 'dotenv'
 
 dotenv.config() // load env vars from .env
