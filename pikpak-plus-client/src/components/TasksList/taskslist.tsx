80ca1fc72c10d94e051dcfbe6fb661192b82bc01
theUniversityFinder <ganjisai19@gmail.com>
2024-02-03 17:27:06 +0530

    minor fix

--- pikpak-plus-client/src/components/TasksList/taskslist.tsx
+++ pikpak-plus-client/src/components/TasksList/taskslist.tsx
@@ -87,7 +87,7 @@ const DownloadList: React.FC = () => {
     }
 
     fetchDownloadData1()
-  }, [])
+  }, [selectedTask])
 
   const handleSelectedValue = async (value) => {
     setSelectedValue(value)
